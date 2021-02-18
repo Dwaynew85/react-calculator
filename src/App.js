c6030ae210fa6596e20cbac5f07dbf7c0f0f19cd
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-02-18 03:29:16 -0500

    replaced functional component with class componenet to better manage state

--- src/App.js
+++ src/App.js
@@ -1,41 +1,61 @@
+import React, { Component } from 'react'
 import './App.css';
 import Button from './components/Button';
 import Input from './components/Input';
+import ClearButton from './components/ClearButton'
 
-function App() {
-  return (
-    <div className="App">
-      <div className="calc-wrapper">
-        <div className="row">
-          <Input></Input>
-        </div>
-        <div className="row">
-          <Button>7</Button>
-          <Button>8</Button>
-          <Button>9</Button>
-          <Button>/</Button>
-        </div>
-        <div className="row">
-          <Button>4</Button>
-          <Button>5</Button>
-          <Button>6</Button>
-          <Button>*</Button>
-        </div>
-        <div className="row">
-          <Button>1</Button>
-          <Button>2</Button>
-          <Button>3</Button>
-          <Button>+</Button>
-        </div>
-        <div className="row">
-          <Button>.</Button>
-          <Button>0</Button>
-          <Button>=</Button>
-          <Button>-</Button>
+export default class App extends Component {
+  state = {
+    input: "",
+    previousNumber: "",
+    currentNumber: "",
+    operator: ""
+  }
+
+  addToInput = val => {
+    this.setState({ input: this.state.input + val })
+  }
+
+  addZeroToInput = val => {
+    
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="calc-wrapper">
+          <div className="row">
+            <Input>{this.state.input}</Input>
+          </div>
+          <div className="row">
+            <Button handleClick={this.addToInput}>7</Button>
+            <Button handleClick={this.addToInput}>8</Button>
+            <Button handleClick={this.addToInput}>9</Button>
+            <Button>/</Button>
+          </div>
+          <div className="row">
+            <Button handleClick={this.addToInput}>4</Button>
+            <Button handleClick={this.addToInput}>5</Button>
+            <Button handleClick={this.addToInput}>6</Button>
+            <Button handleClick={this.addToInput}>*</Button>
+          </div>
+          <div className="row">
+            <Button handleClick={this.addToInput}>1</Button>
+            <Button handleClick={this.addToInput}>2</Button>
+            <Button handleClick={this.addToInput}>3</Button>
+            <Button handleClick={this.addToInput}>+</Button>
+          </div>
+          <div className="row">
+            <Button>.</Button>
+            <Button handleClick={this.addZeroToInput}>0</Button>
+            <Button>=</Button>
+            <Button>-</Button>
+          </div>
+          <div className="row">
+            <ClearButton>Clear</ClearButton>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default App;
