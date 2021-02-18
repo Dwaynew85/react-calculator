6957c6ae1cd1e108807a034c0ae907986af596e3
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-02-18 01:50:28 -0500

    removed unnecessary elements and css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calc-wrapper">
+        <div className="row">
+          <Button>7</Button>
+          <Button>8</Button>
+          <Button>9</Button>
+          <Button>/</Button>
+        </div>
+        <div className="row">
+          <Button>4</Button>
+          <Button>5</Button>
+          <Button>6</Button>
+          <Button>*</Button>
+        </div>
+        <div className="row">
+          <Button>1</Button>
+          <Button>2</Button>
+          <Button>3</Button>
+          <Button>+</Button>
+        </div>
+        <div className="row">
+          <Button>.</Button>
+          <Button>0</Button>
+          <Button>=</Button>
+          <Button>-</Button>
+        </div>
+      </div>
     </div>
   );
 }
