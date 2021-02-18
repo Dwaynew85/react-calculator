4c5f668d92b43e1402033bb6a682fa717a19cd3b
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-02-18 02:12:51 -0500

    added deconstructor to props

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import './Button.css'
 
-export default function Button(props) {
+export default function Button({ children }) {
     const isOperator = val => {
         return !isNaN(val) || val === "." || val === "=";
     }
 
     return (
         <div 
-            className={`button ${isOperator(props.children) ? "" : "operator"}`}
+            className={`button ${isOperator(children) ? "" : "operator"}`}
         >
-           {props.children} 
+           {children} 
         </div>
     )
 }
