5d3905684c834eb29da3d1bab4fa7722fbc21152
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-02-18 02:03:00 -0500

    added styling for operator buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,8 +2,14 @@ import React from 'react'
 import './Button.css'
 
 export default function Button(props) {
+    const isOperator = val => {
+        return !isNaN(val) || val === "." || val === "=";
+    }
+
     return (
-        <div className="button">
+        <div 
+            className={`button ${isOperator(props.children) ? "" : "operator"}`}
+        >
            {props.children} 
         </div>
     )
